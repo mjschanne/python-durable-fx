e95f0cb9c8da663f9eac1b31192f453655d62f8e
MJ Schanne <mjschanne@microsoft.com>
2023-10-15 23:49:19 -0400

    Add durable function basis

--- function_app.py
+++ function_app.py
@@ -1,7 +1,8 @@
 import azure.functions as func
+import azure.durable_functions as df
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
@@ -23,3 +24,27 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="blobadded", connection="EventHubConnectionString") 
+def eventhub_trigger(azeventhub: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s', azeventhub.get_body().decode('utf-8'))
+
+@app.route(route="orchestrators/{functionName}")
+@app.durable_client_input(client_name="client")
+async def http_start(req: func.HttpRequest, client):
+    function_name = req.route_params.get('functionName')
+    instance_id = await client.start_new(function_name)
+    
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    return client.create_check_status_response(req, instance_id)
+
+@app.orchestration_trigger(context_name="context")
+def my_orchestrator(context: df.DurableOrchestrationContext):
+    result1 = yield context.call_activity('say_hello', "Tokyo")
+    result2 = yield context.call_activity('say_hello', "Seattle")
+    result3 = yield context.call_activity('say_hello', "London")
+    return [result1, result2, result3]
+
+@app.activity_trigger(input_name="city")
+def say_hello(city: str) -> str:
+    return f"Hello {city}!"
